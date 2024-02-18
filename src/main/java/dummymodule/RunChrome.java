ad764427cbed79423c3dad932ea35d88f08d7e23
ShahriarHHasan <shantoneel1203@gmail.com>
2024-02-18 12:21:45 +0600

    first comment

---
+++ src/main/java/dummymodule/RunChrome.java
@@ -0,0 +1,10 @@
+package dummymodule;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RunChrome {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+    }
+}
